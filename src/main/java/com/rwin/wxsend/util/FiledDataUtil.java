6a3ccb5c0b1aa7a6e49249582b8592595b02cdc1
zhw1998 <1562813192@qq.com>
2022-12-18 21:47:06 +0800

    微信消息推送后台接口代码完善

--- src/main/java/com/rwin/wxsend/util/FiledDataUtil.java
+++ src/main/java/com/rwin/wxsend/util/FiledDataUtil.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,7 +24,8 @@ public class FiledDataUtil {
 
 
     static {
-        filedMap.put("生日", "birthday");
+        filedMap.put("距离生日天数", "arriveBirthdayDays");
+        filedMap.put("天气", "weather");
     }
 
     /**
@@ -45,6 +47,9 @@ public class FiledDataUtil {
                     method = m;
                 }
             }
+            if(method == null){
+                return "";
+            }
             return (String) method.invoke(filedUtilClass, modelFiled.getValue());
         }catch (Exception e){
             log.error("获取参数【{}】数据失败:{}", modelFiled.getKey(), e);
@@ -53,6 +58,34 @@ public class FiledDataUtil {
     }
 
 
+    /**
+     * 获取距离生日天数
+     * @param value
+     * @return
+     */
+    public String getArriveBirthdayDays(String value){
+        Date birthday = DateUtil.convertString2Date(value);
+        return DateUtil.diffDay(birthday, new Date())+"";
+    }
+
+
+    /**
+     * 获取天气
+     * @param city
+     * @return
+     */
+    public String getWeather(String city){
+        return "";
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Date birthday = DateUtil.convertString2Date("2023-12-1");
+        System.out.print(DateUtil.diffDay(birthday, new Date()));
+    }
 
 
 
